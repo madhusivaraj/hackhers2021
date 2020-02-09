d4534ccae378c5d45ea3924c30c1d63060cc5e5e
Madhu Sivaraj <madhusivaraj22@gmail.com>
2020-02-09 00:29:27 -0500

    2021 site redesign

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.faq li .question').click(function () {
+    $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+    $(this).parent().toggleClass('active');
+  });
+});
